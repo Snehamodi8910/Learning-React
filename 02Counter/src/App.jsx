439493514697a2d19eb6f3942eb7bb6315ac31f9
Snehamodi8910 <sneha.modi.cer22@itbhu.ac.in>
2024-06-26 02:33:09 +0530

    changes commited

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -4,22 +4,33 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-let counter = 5;
-const addValue = ()=>{
-    console.log("clicked" , counter);
+  let [counter , setCounter] = useState(5)
 
-    counter = counter + 1;
+  const addValue = ()=>{
+
+
+    //counter = counter + 1;
+    if(counter<20)
+    {setCounter(counter+1)}
 }
+
+  const removeValue= ()=>{
+    if(counter > 0)
+      {
+        setCounter(counter-1)
+      }
+
+  }
   return (
     <>
       <h1>Chai aur React</h1>
       <h2>Counter Value : {counter}</h2>
 
-      <button onClick={addValue}>Add Value{counter}</button>
+      <button onClick={addValue}> Add Value: {counter}</button>
       <br/>
       <br/>
-      <button>Remove Value {counter}</button>
-      <p>footer : {counter}</p>
+      <button onClick={removeValue}>Remove Value: {counter}</button>
+      <p>Footer : {counter}</p>
 
     </>
   )
