f40f6b2419db2dd27d32ebf87afa9275f82db870
Snehamodi8910 <sneha.modi.cer22@itbhu.ac.in>
2024-06-26 01:59:20 +0530

    commited

--- 01_vite_project/src/main.jsx
+++ 01_vite_project/src/main.jsx
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+function myApp(){
+  return(
+    <div>
+      <h1>custom app ! </h1>
+    </div>
+  )
+}
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
